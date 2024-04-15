0da7e995b1c78a2161bb02232d97b8456add998e
Phạm Cường QUốc <167053034+pcq3014@users.noreply.github.com>
2024-04-15 20:07:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,8 +59,4 @@ plt.xlabel('genre')
 plt.ylabel('budget')
 plt.title('Matplotlib Bar Chart Showing The Average Budget of Movies in Each Genre')
 st.pyplot(fig)
-hide_github_icon = """
 
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
